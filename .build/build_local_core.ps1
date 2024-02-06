6e960c09b487596f96c3600088e5dee61f2f7066
Dmytro Trofimchuk <dmytro.trofimchuk@acupower.co.uk>
2024-02-06 21:38:26 +0200

    add def sitemap, update icons

--- .build/build_local_core.ps1
+++ .build/build_local_core.ps1
@@ -3,4 +3,4 @@
 )
 
 & ".\2_1_prepare_output_core.ps1"
-& ".\3_build_package.ps1" -srcPath "output" -packageName "DTInitial.zip" -version $version -packageLevel 2
+& ".\3_build_package.ps1" -srcPath "output" -packageName "DTInitial.zip" -version $version
